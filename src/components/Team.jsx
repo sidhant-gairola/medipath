0443f7e48cd65a3b7d3c512823947e3d71981f82
Sidhant Gairola <ssid7074@gmail.com>
2025-10-22 20:12:15 +0530

    Minor updates including axios version update & small team style adjustment

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -29,14 +29,14 @@ function Team() {
           {/* Meet Our Team */}
           About Us
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2  justify-items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center">
           {teamMembers.map((member, index) => (
             <motion.div
               key={index}
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: index * 0.1 }}
-              className="max-w-xl bg-white hover:bg-neutral-100 rounded-2xl shadow-lg overflow-hidden" >
+              className="max-w-xl mb-5 bg-white hover:bg-neutral-100 rounded-2xl shadow-lg overflow-hidden" >
               <img src={member.image} alt={member.name} className="w-96 h-96 object-cover hover:scale-105 transition-all duration-500" />
               <div className="p-6">
                 <h3 className="text-2xl font-semibold mb-2 text-black">{member.name}</h3>
